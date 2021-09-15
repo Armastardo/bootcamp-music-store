eacc2c2e6ffc4a43e16702dd8a8b38c578b1235a
Brandon <brandon.abr@hotmail.com>
2021-09-15 02:10:23 -0500

    Initial commit, proyecto en blanco.

---
+++ IntelliJ Project/src/test/java/com/globant/musicstore/MusicStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.musicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
